90d2a3f4b9c18244e367c88b5f42bbf045de7d2b
albertuchr <albertuschristianto27@gmail.com>
2025-11-21 16:33:35 +0700

    Commit No. 3 – Implement Shared ViewModel in Fragment

--- app/src/main/java/com/example/lab_week_10/TotalFragment.kt
+++ app/src/main/java/com/example/lab_week_10/TotalFragment.kt
@@ -1,34 +1,44 @@
 package com.example.lab_week_10
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.Observer
+import com.example.lab_week_10.viewmodels.TotalViewModel
 
 class TotalFragment : Fragment() {
 
     private lateinit var textTotal: TextView
     private lateinit var buttonIncrement: Button
-    private var total = 0
+
+    private val viewModel by lazy {
+        ViewModelProvider(requireActivity())[TotalViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_total, container, false)
+        return inflater.inflate(R.layout.fragment_total, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         textTotal = view.findViewById(R.id.text_total)
         buttonIncrement = view.findViewById(R.id.button_increment)
 
-        buttonIncrement.setOnClickListener {
-            total++
-            textTotal.text = getString(R.string.text_total, total)
-        }
+        // LiveData Observe Here
+        viewModel.total.observe(viewLifecycleOwner, Observer { value ->
+            textTotal.text = getString(R.string.text_total, value)
+        })
 
-        return view
+        buttonIncrement.setOnClickListener {
+            viewModel.incrementTotal()
+        }
     }
 }
--- app/src/main/java/com/example/lab_week_10/viewmodels/TotalViewModel.kt
+++ app/src/main/java/com/example/lab_week_10/viewmodels/TotalViewModel.kt
@@ -1,12 +1,15 @@
 package com.example.lab_week_10.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class TotalViewModel : ViewModel() {
-    var total: Int = 0
 
-    fun incrementTotal(): Int {
-        total++
-        return total
+    private val _total = MutableLiveData(0)
+    val total: LiveData<Int> = _total
+
+    fun incrementTotal() {
+        _total.value = (_total.value ?: 0) + 1
     }
 }
